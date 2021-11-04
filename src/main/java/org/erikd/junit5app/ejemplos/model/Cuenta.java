67d0055f67de5db1780413d6b77726f5e1aa7e60
dellmdq <erikdell@gmail.com>
2021-11-04 14:59:49 -0300

    Assertions. ThrowsException

--- src/main/java/org/erikd/junit5app/ejemplos/model/Cuenta.java
+++ src/main/java/org/erikd/junit5app/ejemplos/model/Cuenta.java
@@ -1,5 +1,7 @@
 package org.erikd.junit5app.ejemplos.model;
 
+import org.erikd.junit5app.ejemplos.exceptions.DineroInsuficienteException;
+
 import java.math.BigDecimal;
 
 public class Cuenta {
@@ -26,5 +28,26 @@ public class Cuenta {
 
     public void setSaldo(BigDecimal saldo) {
         this.saldo = saldo;
+    }
+
+    public void debito(BigDecimal monto){
+        BigDecimal nuevoSaldo = this.saldo.subtract(monto); //hay que asignarlo a una variable este metodo retorna otro big decimal con el resultado
+        if(nuevoSaldo.compareTo(BigDecimal.ZERO) < 0) throw new DineroInsuficienteException("Dinero insuficiente");
+        this.saldo = nuevoSaldo;
+    }
+
+    public void credito(BigDecimal monto){
+        this.saldo = this.saldo.add(monto);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Cuenta)) return false; //instance of tambien valida si es nulo o no... obj == null estaría de más
+
+        Cuenta c = (Cuenta) obj;
+
+        if(this.persona == null || this.saldo == null) return false;
+
+        return this.persona.equals(c.getPersona()) && this.saldo.equals(c.getSaldo());
     }
 }
--- src/test/java/org/erikd/junit5app/ejemplos/model/CuentaTest.java
+++ src/test/java/org/erikd/junit5app/ejemplos/model/CuentaTest.java
@@ -1,5 +1,6 @@
 package org.erikd.junit5app.ejemplos.model;
 
+import org.erikd.junit5app.ejemplos.exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -16,12 +17,61 @@ class CuentaTest {
     void testNombreCuenta(){
         Cuenta cuenta = new Cuenta("Pepe Albistur",new BigDecimal("1000.12345"));
         //cuenta.setPersona("Pepe Albistur");
-        String esperado = "Pepe Albistu";
-
-        //assertEquals(esperado,cuenta.getPersona());
+        String esperado = "Pepe Albistur";
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(esperado,cuenta.getPersona());
         assertTrue(cuenta.getPersona().equals(esperado));
 
     }
 
+    @Test
+    void testSaldoCuenta(){
+        Cuenta cuenta = new Cuenta("Pepe", new BigDecimal("1000.12345"));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1000.12345,cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);//chequendo que la cuenta tenga saldo mayor a cero
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);//validando lo mismo al revés. chequendo que la cuenta tenga saldo mayor a cero
+    }
 
+    @Test
+    void testReferenciaCuenta() {//testeando por instancia
+        Cuenta cuenta = new Cuenta("John Doe", new BigDecimal("8900.9998"));//real
+        Cuenta cuenta2 = new Cuenta("John Doe", new BigDecimal("8900.9998"));//esperado
+        assertNotNull(cuenta.getSaldo());
+//        assertNotEquals(cuenta2, cuenta);
+        assertEquals(cuenta2,cuenta);//falla porque son dos instancias distintas
+    }
+    /***
+     *  Aqui vemos primero el test y después el desarrollo necesario en el metodo
+     *  para cumplir con el resultado exigido por el método. Esto es Test Driven Development.TDD.
+     *  Falta la implementacion de los metodos.
+     */
+    @Test
+    void testDebitoCuenta(){
+        Cuenta cuenta = new Cuenta("Marce Pala", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345",cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testCreditoCuenta(){
+        Cuenta cuenta = new Cuenta("Marce Pala", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100, cuenta.getSaldo().intValue());
+        assertEquals("1100.12345",cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testDineroInsuficienteExceptionCuenta() {
+        Cuenta cuenta = new Cuenta("Marce Pala",new BigDecimal("1000.12345"));
+        Exception exception = assertThrows(DineroInsuficienteException.class, () -> {
+           cuenta.debito(new BigDecimal(1500));
+        });
+        String actual = exception.getMessage();
+        String esperado = "Dinero insuficiente";
+        assertEquals(esperado, actual);
+    }
 }
